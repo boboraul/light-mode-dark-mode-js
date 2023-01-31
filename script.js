729d40aa9c15b5dcb8074ec1f004768b90490905
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2023-01-31 15:03:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,12 +26,12 @@ function toggleDarkLightMode(isDark) {
 // Swicth Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', DARK_THEME);
+        localStorage.setItem('theme', DARK_THEME);
         toggleDarkLightMode(true);
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
+        document.documentElement.setAttribute('data-theme', LIGHT_THEME);
+        localStorage.setItem('theme', LIGHT_THEME);
         toggleDarkLightMode(false);
     }
 }
@@ -43,7 +43,7 @@ toggleSwitch.addEventListener('change', switchTheme);
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
+    if (currentTheme === DARK_THEME) {
         toggleSwitch.checked = true;
         toggleDarkLightMode(true);
     }
